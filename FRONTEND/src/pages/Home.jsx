88d0f1c8a521a6afeec46ef4a1a6d322868f98b6
Bahavathy B <bahavathyb@gmail.com>
2025-09-02 17:17:45 +0530

    Update Home.jsx

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Hero from '../components/Hero'
 import PopularItems from '../components/PopularItems'
 import Offers from '../components/Offers'
 import NewCollections from '../components/NewCollections'
 import NewsLetter from '../components/NewsLetter'
+import { jwtDecode } from 'jwt-decode'
 
 
 const Home = () => {
 
+  useEffect(() => {
+    let token = localStorage.getItem("auth_token");
+    console.log(token);
+
+    if(token!== null){
+    const decoded = jwtDecode(token);
+    const role = decoded.role;
+    if(role == "admin"){
+      localStorage.removeItem("auth_token");
+    }
+    }
+    
+  }, []);
 
   return (
     <div>
